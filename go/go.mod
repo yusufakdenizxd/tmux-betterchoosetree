dcbe45300001ab1f8ef75bac2f264b8c84dac586
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-07-24 17:22:20 +0300

    changed to tcell ui lib

--- go/go.mod
+++ go/go.mod
@@ -4,4 +4,15 @@ go 1.22.4
 
 require github.com/nsf/termbox-go v1.1.1
 
-require github.com/mattn/go-runewidth v0.0.9 // indirect
+require (
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell v1.4.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.4 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- go/main.go
+++ go/main.go
@@ -2,61 +2,93 @@ package main
 
 import (
 	"bufio"
-
 	"os/exec"
+	"sort"
 	"strconv"
 	"strings"
 
-	"github.com/nsf/termbox-go"
+	"github.com/gdamore/tcell/v2"
 )
 
-func initUI() error {
-	return termbox.Init()
+type session struct {
+	index    int
+	name     string
+	attached bool
 }
 
-func closeUI() {
-	termbox.Close()
+type ByIndex []session
+
+func (m ByIndex) Len() int           { return len(m) }
+func (m ByIndex) Less(i, j int) bool { return m[i].index < m[j].index }
+func (m ByIndex) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
+
+func initUI() (tcell.Screen, error) {
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		return nil, err
+	}
+	if err := screen.Init(); err != nil {
+		return nil, err
+	}
+	return screen, nil
 }
 
-func drawWindows(windows []string, selected int) {
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+func closeUI(screen tcell.Screen) {
+	screen.Fini()
+}
 
-	cols, _ := termbox.Size()
+func drawWindows(screen tcell.Screen, windows []string, selected int) {
+	screen.Clear()
+
+	cols, _ := screen.Size()
 	for i, window := range windows {
-		fg := termbox.ColorLightGray
-		bg := termbox.ColorDefault
+		fg := tcell.ColorLightGray
+		bg := tcell.ColorBlack
 		if i == selected {
-			fg = termbox.ColorBlack
-			bg = termbox.ColorLightGray
+			fg = tcell.ColorBlack
+			bg = tcell.ColorLightGray
 		}
 		indexStr := "(" + strconv.Itoa(i) + ") " + window
 
 		for j, ch := range indexStr {
 			if j < cols {
-				termbox.SetCell(j, i, ch, fg, bg)
+				screen.SetContent(j, i, ch, nil, tcell.StyleDefault.Foreground(fg).Background(bg))
 			}
 		}
 
 		for j := len(indexStr); j < cols; j++ {
-			termbox.SetCell(j, i, ' ', fg, bg)
+			screen.SetContent(j, i, ' ', nil, tcell.StyleDefault.Foreground(fg).Background(bg))
 		}
 	}
-	termbox.Flush()
+	screen.Show()
 }
 
-func getTmuxSessions() ([]string, error) {
-	cmd := exec.Command("tmux", "list-sessions", "-F", "#S")
+func getTmuxSessions() ([]session, error) {
+	cmd := exec.Command("tmux", "list-sessions", "-F", "#{session_id}~#{session_name}~#{session_attached}")
 	output, err := cmd.Output()
 	if err != nil {
 		return nil, err
 	}
 
-	var sessions []string
+	var sessionInfos []string
 	scanner := bufio.NewScanner(strings.NewReader(string(output)))
 	for scanner.Scan() {
-		sessions = append(sessions, scanner.Text())
+		sessionInfos = append(sessionInfos, scanner.Text())
 	}
-	return sessions, scanner.Err()
+	var retDate []session
+	for _, sesData := range sessionInfos {
+		splitted := strings.Split(sesData, "~")
+		index, _ := strconv.Atoi(strings.Replace(splitted[0], "$", "", 1))
+		attached := false
+		if splitted[2] == "1" {
+			attached = true
+		}
+
+		retDate = append(retDate, session{index: index, name: splitted[1], attached: attached})
+	}
+
+	sort.Sort(ByIndex(retDate))
+	return retDate, scanner.Err()
 }
 
 func getTmuxWindows() ([]string, error) {
@@ -67,7 +99,7 @@ func getTmuxWindows() ([]string, error) {
 
 	var allWindows []string
 	for _, session := range sessions {
-		cmd := exec.Command("tmux", "list-windows", "-t", session, "-F", "#W")
+		cmd := exec.Command("tmux", "list-windows", "-t", session.name, "-F", "#{window_name}")
 		output, err := cmd.Output()
 		if err != nil {
 			return nil, err
@@ -80,11 +112,21 @@ func getTmuxWindows() ([]string, error) {
 		}
 
 		if len(windows) == 1 {
-			allWindows = append(allWindows, "- "+session+" - "+windows[0])
+			commands = append(commands, session.name)
+			name := "- " + session.name + " -> " + windows[0]
+			if session.attached {
+				name = name + " (attached)"
+			}
+			allWindows = append(allWindows, name)
 		} else {
-			allWindows = append(allWindows, "- "+session+": "+strconv.Itoa(len(windows))+" windows")
-			for _, win := range windows {
-
+			name := "-" + session.name + ": " + strconv.Itoa(len(windows)) + " windows"
+			if session.attached {
+				name = name + " (attached)"
+			}
+			allWindows = append(allWindows, name)
+			commands = append(commands, session.name)
+			for windowIndex, win := range windows {
+				commands = append(commands, session.name+":"+strconv.Itoa(windowIndex+1))
 				allWindows = append(allWindows, "--> "+win)
 			}
 		}
@@ -93,13 +135,14 @@ func getTmuxWindows() ([]string, error) {
 	return allWindows, nil
 }
 
-func main() {
+var commands []string
 
-	err := initUI()
+func main() {
+	screen, err := initUI()
 	if err != nil {
 		panic(err)
 	}
-	defer closeUI()
+	defer closeUI(screen)
 
 	var rows []string
 	windows, err := getTmuxWindows()
@@ -110,41 +153,63 @@ func main() {
 
 	selected := 0
 	for {
-		drawWindows(rows, selected)
+		drawWindows(screen, rows, selected)
 
-		switch ev := termbox.PollEvent(); ev.Type {
-		case termbox.EventKey:
-			if ev.Ch == 'q' || ev.Key == termbox.KeyEsc {
+		event := screen.PollEvent()
+		switch event.(type) {
+		case *tcell.EventKey:
+
+			key := event.(*tcell.EventKey)
+			switch key.Rune() {
+			case 'q':
 				return
-			}
-			if ev.Ch == 'j' {
+			case 'n':
+				//TODO: Open tmuxinator or zoxide session creation popup
+			case 'j':
 				selected++
 				if selected >= len(rows) {
 					selected = 0
 				}
-			} else if ev.Ch == 'k' {
+			case 'k':
 				selected--
 				if selected < 0 {
 					selected = len(rows) - 1
 				}
-			} else if ev.Ch == 'g' {
+			case 'g':
 				selected = 0
-			} else if ev.Ch == 'G' {
+			case 'G':
 				selected = len(rows) - 1
-			} else if ev.Key == termbox.KeyEnter {
-
-				cmd := exec.Command("tmux", "switch", "-t", "tmux-betterchoosetree"+":"+strconv.Itoa(selected))
+			}
+			switch key.Key() {
+			case tcell.KeyEsc, tcell.KeyCtrlC:
+				return
+			case tcell.KeyDown:
+				selected++
+				if selected >= len(rows) {
+					selected = 0
+				}
+			case tcell.KeyUp:
+				selected--
+				if selected < 0 {
+					selected = len(rows) - 1
+				}
+			case tcell.KeyCtrlK:
+				selected--
+				if selected < 0 {
+					selected = len(rows) - 1
+				}
+			case tcell.KeyHome:
+				selected = 0
+			case tcell.KeyEnd:
+				selected = len(rows) - 1
+			case tcell.KeyEnter:
+				cmd := exec.Command("tmux", "switch", "-t", commands[selected])
 				_, err := cmd.Output()
-
 				if err != nil {
 					panic(err)
 				}
 				return
-				// exec.Command("tmux", "switch-client", "-t", "tmux-betterchoosetree:1")
-				// return
 			}
-		case termbox.EventError:
-			panic(ev.Err)
 		}
 	}
 }
